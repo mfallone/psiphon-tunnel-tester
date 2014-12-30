9c1e6ba99dbc118792995c5c96da96bdb7ca5405
mfallone <mike.fallone@gmail.com>
2014-12-30 11:35:54 -0500

    refactor and clean up.

--- tunnelTests.go
+++ tunnelTests.go
@@ -56,7 +56,6 @@ func getExternalIPAddress(site string, client *http.Client) (ip net.IP, err erro
 	if ip == nil {
 		log.Println("Could not parse IP")
 	}
-	log.Println(ip)
 
 	return
 }
@@ -65,7 +64,8 @@ func getExternalIPAddress(site string, client *http.Client) (ip net.IP, err erro
 // download it to a local file in the current directory.
 // The intent of this test is to check the timing of a tunneled vs untunneled
 // file download
-func downloadFile(site string, client *http.Client, done chan bool) {
+// TODO the `done` chan should be replaced with one that reports the time duration.  Possibly []bytes
+func downloadFile(site string, outpath string, client *http.Client, done chan string) {
 	// a channel will be needed to signal when complete
 
 	startTime := time.Now()
@@ -76,15 +76,16 @@ func downloadFile(site string, client *http.Client, done chan bool) {
 	}
 	defer resp.Body.Close()
 
-	outfile, err := os.Create("LRGOutFile.bin")
+	if outpath == "" {
+		outpath = "./tmp/LRGOutFile.bin"
+	}
+	outfile, err := os.Create(outpath)
 	if err != nil {
 		log.Printf("Error creating file: %s", err)
 	}
 	io.Copy(outfile, resp.Body)
-	duration := time.Now().Sub(startTime)
-	log.Printf("Download file duration: %v", duration)
-
-	done <- true
+	duration := time.Now().Sub(startTime).String()
+	done <- duration
 }
 
 // SetupTasks is called by the main function.  It prepares and runs the tasks
@@ -93,9 +94,11 @@ func SetupTasks(config *psiphon.Config, decodedServerEntry *psiphon.ServerEntry,
 	log.Println("Setting up Tasks")
 
 	untunneled := new(TasksResults)
+	untunneled.Label = "UNTUNNELED"
 	untunneled.done = make(chan bool, 1)
 
 	httpTunneled := new(TasksResults)
+	httpTunneled.Label = "HTTPTUNNEL"
 	httpTunneled.done = make(chan bool, 1)
 
 	proxyConfig := setProxyConfig("127.0.0.1", 8080, false)
@@ -120,14 +123,13 @@ func SetupTasks(config *psiphon.Config, decodedServerEntry *psiphon.ServerEntry,
 	go untunneled.Run(tasksConfig, proxyConfig)
 	go httpTunneled.Run(tasksConfig, proxyConfig)
 
-	<-httpTunneled.done
 	<-untunneled.done
+	<-httpTunneled.done
+
 	log.Println("Tests Completed")
 
 	log.Printf("Untunneled IP: %s", untunneled.externalIP)
 	log.Printf("Tunneled IP: %s", httpTunneled.externalIP)
-
-	// go tunneled.Run(config, decodedServerEntry, tasksConfig)
 
 }
 
@@ -148,20 +150,21 @@ func (tasks *TasksResults) Run(tasksConfig TasksConfig, proxyConfig ProxyConfig)
 	}
 
 	// Get a web resource we know serves up our external IP address as a page
-	log.Println("Checking external IP address")
+	log.Printf("%s: Checking external IP address..", tasks.Label)
 	external_ip, err := getExternalIPAddress(tasksConfig.ExternalIPCheckSite, client)
 	if err != nil {
-		log.Fatalf("Error getting Exterinal IP: %s", err)
+		log.Fatalf("%s: Error getting Exterinal IP: %s", tasks.Label, err)
 	}
 	tasks.externalIP = external_ip
 
 	// Start file download test
-	//TODO should be done in a go routine so state can be checked while it's running
-	done := make(chan bool, 1)
-	go downloadFile(tasksConfig.LRGDownloadFile, client, done)
-	log.Print("Large file download started")
-	<-done
-	log.Println("....completed")
+	tasks.largeDownloadFileTime = make(chan string, 1)
+	outfile := "./tmp/" + tasks.Label + ".bin"
+
+	go downloadFile(tasksConfig.LRGDownloadFile, outfile, client, tasks.largeDownloadFileTime)
+
+	log.Printf("%s: Large file download started", tasks.Label)
+	log.Printf("%s: Large file download complete: %s", tasks.Label, <-tasks.largeDownloadFileTime)
 
 	tasks.done <- true
 }
